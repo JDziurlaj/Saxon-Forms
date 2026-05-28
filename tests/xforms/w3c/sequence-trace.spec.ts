1e15253d7b7c66529d6f2228a8a5f1bd15d1f902
John Dziurlaj <john@hiltonroscoe.com>
2026-05-28 12:44:55 -0400

    Create sequence-trace.spec.ts

---
+++ tests/xforms/w3c/sequence-trace.spec.ts
@@ -0,0 +1,8 @@
+import { test } from "@playwright/test";
+
+test.describe("W3C sequence trace checkpoints", () => {
+  test.skip(
+    "sequence trace validation scaffold",
+    "Reserved for sequence-diagram trace assertions referenced by docs/docbook/checkpoints/sequence-trace-spec.json."
+  );
+});
